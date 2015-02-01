b7c54a0253f7d30c4f1198d3cf39c34aa1e6249d
Titus Soporan <titus@tsoporan.com>
2015-01-31 23:05:59 -0500

    update title and desc

--- go/euler-1.go
+++ go/euler-1.go
@@ -1,5 +1,5 @@
-// Title       : Euler Project Problem 
-// Description : Find the sum of all the multiples of 3 or 5 below 1000.
+// Title       : Euler Project Problem 1
+// Description : Find the sum of all the multiples of 3,5,6,9  below 1000.
 
 package main
 
